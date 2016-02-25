eae948213ec5a48cbb3ee34df039a8ff93f15551
liziqiang <lzq.hdu@gmail.com>
2016-02-25 22:05:36 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,15 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):  # 1
+class NewVisitorTest(LiveServerTestCase):
 
-    def setUp(self):  # 2
+    def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):  # 3
+    def tearDown(self):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
@@ -17,10 +17,10 @@ class NewVisitorTest(unittest.TestCase):  # 1
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  # 4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -58,7 +58,3 @@ class NewVisitorTest(unittest.TestCase):  # 1
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == '__main__':  # 7
-    unittest.main(warnings='ignore')  # 8
